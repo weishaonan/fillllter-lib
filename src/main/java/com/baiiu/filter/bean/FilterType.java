109544f6b7654130d1d5750b2e0bcc9ea19f60ec
weishaonan <1320843016@qq.com>
2018-07-05 21:43:57 +0800

    Initial commit

---
+++ src/main/java/com/baiiu/filter/bean/FilterType.java
@@ -0,0 +1,11 @@
+package com.baiiu.filter.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/25.
+ */
+public class FilterType {
+    public String desc;
+    public List<String> child;
+}
